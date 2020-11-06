56366851458e5f40c781a81616e6a1fb12c941bc
Alessandro Vinci <alessandrovinciabc@gmail.com>
2020-11-06 09:42:08 +0100

    Add listener, basic ui functionality

--- main.js
+++ main.js
@@ -46,16 +46,98 @@ let game = (function(){
             return [..._squares];
         }
 
-        return {log};
+        function setSquare(pos, value){
+            let normalized, result;
+
+            normalized = value.toUpperCase();
+
+            if(pos >= 0 && pos < _squares.length){
+                switch(normalized){
+                    case 'X':
+                        _squares[pos] = value;
+                        result = true;
+                        break;
+                    case 'O':
+                        _squares[pos] = value;
+                        result = true;
+                        break;
+                    default:
+                        result = false;
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        return {log, getBoard, setSquare};
     })();
 
     console.log(gameBoard.log());
 })();
 
 let ui = (function(){
+    let _DOM, _playerSymbol;
 
+    _DOM = {
+        board: document.querySelector('.board'),
+    };
+
+    _playerSymbol = 'X';
+
+    function _validateInput(input){
+        let normalized;
+        normalized = input.toUpperCase();
+
+        switch(normalized){
+            case 'X':
+            case 'O':
+                break;
+            default:
+                return false;
+        }
+
+        return normalized;
+    }
+
+    function _handleClick(e){
+        let isSquare;
+
+        isSquare = e.target.classList.contains('square');
+
+        if(isSquare && e.target.textContent === ''){
+            e.target.textContent = _playerSymbol;
+            toggleSymbol();
+        }
+    }
+
+    function toggleSymbol(){
+        return _playerSymbol = _playerSymbol === 'X' ? 'O' : 'X';
+    }
+
+    function setSymbol(character){
+        let validated;
+
+        validated = _validateInput(character);
+        if(validated){
+            _playerSymbol = validated;
+        }
+    }
+
+    function setUpListeners(){
+        _DOM.board.addEventListener('click', _handleClick);
+    }
+
+    return {setUpListeners, toggleSymbol, setSymbol};
 })();
 
-let controller = (function(){
+let controller = (function(data, view){
 
-})();
+    function init(){
+        view.setUpListeners();
+    }
+
+    return {init};
+})(game, ui);
+
+controller.init();
